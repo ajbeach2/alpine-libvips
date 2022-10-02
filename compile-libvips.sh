6a512df49721aa548e111123f386d895d5d990fd
Alex Beach <ajbeach2@gmail.com>
2022-10-02 14:42:18 -0400

    updatge to 8.13.2

--- compile-libvips.sh
+++ compile-libvips.sh
@@ -1,7 +1,7 @@
 cd /tmp && \
 git clone https://github.com/libvips/libvips.git && \
 cd /tmp/libvips && \
-git checkout v8.10.5 && \
+git checkout v8.13.2 && \
 ./autogen.sh --prefix="$HOME/libvips" --disable-magicksave --disable-magickload && \
 make && \
 make install
